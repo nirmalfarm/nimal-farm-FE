bcbf814e7dd1b45ad792b0599ac99ffeba4d4df5
Yadnyesh Khotre <96002336+yadnyeshkhotre@users.noreply.github.com>
2025-07-08 00:29:32 +0530

    Update Axios.js

--- src/API/Axios.js
+++ src/API/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = "http://127.0.0.1:8000/NirmalFarms/api";
+const API = "https://manasdev.pythonanywhere.com/NirmalFarms/api";
 
 const axiosInstance = axios.create({
   baseURL: API,
